10a0a6cac7049a2aaaa57cd024f3379bb87088c7
Sina Zinsaz <sinazinsaz1@gamil.com>
2024-04-05 16:47:51 +0330

    feat: update Categories and responsive

--- src/components/categories/Categories.tsx
+++ src/components/categories/Categories.tsx
@@ -4,12 +4,13 @@ const Categories_data = ['مردانه', 'زنانه' , 'بچگانه' , 'شگف
 
 export default function Categories() {
   return (
-    <Box width='100%' mt='1rem' display='flex' alignContent='center' justifyContent='center' bgcolor='#F3F3F3' border='1px solid #272D4E'>
-    {Categories_data.map((categorie, index) => ( 
-        <Box key={index} m='1rem' px='5px' component='select' fontSize='1.2rem' style={{ direction: 'rtl', boxShadow: 'none', border: 'none', backgroundColor: 'transparent' }}>
-          <option>{categorie}</option>
-        </Box>
-      ))}
-   </Box>
+<Box width='100%' mt='1rem' display='flex' alignContent='center' justifyContent='center' bgcolor='#F3F3F3' border='1px solid #272D4E' borderRight='none' borderLeft='none'>
+  {Categories_data.map((categorie, index) => (
+    <Box key={index} m={{ xs: '0.5rem', md: '1rem' }} px='5px' component='select' fontSize='1.2rem' style={{ direction: 'rtl', boxShadow: 'none', border: 'none', backgroundColor: 'transparent' }}>
+      <option>{categorie}</option>
+    </Box>
+  ))}
+</Box>
+
   )
 }
