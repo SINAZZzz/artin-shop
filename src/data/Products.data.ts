f28a76a0791556f2be2636e9a47daf64408872ec
Sina Zinsaz <sinazinsaz1@gamil.com>
2024-04-10 11:28:38 +0330

    feat : add data products

---
+++ src/data/Products.data.ts
@@ -0,0 +1,52 @@
+export const ProductsData = {
+    "products": [
+      {
+        "id": "0",
+        "title": "کفش اسپرت سارزی",
+        "description": "380,000 تومان",
+        "imageUrl": "https://s8.uupload.ir/files/group_1261153106_(2)_ekey.png"
+      }, 
+      {
+        "id": "1",
+        "title": "کفش پیاده روی  مدل NOV-BK",
+        "description": "360,000 تومان",
+        "imageUrl" : "https://s8.uupload.ir/files/group_1261153104_9c2c.png"
+      },
+      {
+        "id": "2",
+        "title": "کفش راحتی کد 210",
+        "description": "240,000 تومان",
+        "imageUrl" : "https://s8.uupload.ir/files/group_1261153106_(1)_uld1.png"
+      },
+      {
+        "id": "3",
+        "title": "کفش راحتی کد 210",
+        "description": "240,000 تومان",
+        "imageUrl" : "https://s8.uupload.ir/files/group_1261153106_i6d8.png"
+      },
+      {
+        "id": "4",
+        "title": "کفش پیاده روی  مدل NOV-BK",
+        "description": "360,000 تومان",
+        "imageUrl" : "https://s8.uupload.ir/files/group_1261153106_(2)_ekey.png"
+      },
+      {
+        "id": "5",
+        "title": "کفش پیاده روی  مدل NOV-BK",
+        "description": "360,000 تومان",
+        "imageUrl" : "https://s8.uupload.ir/files/group_1261153104_9c2c.png"
+      },
+      {
+        "id": "6",
+        "title": "کفش اسپرت سارزی",
+        "description": "380,000 تومان",
+        "imageUrl" : "https://s8.uupload.ir/files/group_1261153106_(1)_uld1.png"
+      },
+      {
+        "id": "7",
+        "title": "کفش راحتی کد 210",
+        "description": "240,000 تومان",
+        "imageUrl" : "https://s8.uupload.ir/files/group_1261153106_i6d8.png"
+      },
+    ],
+}
