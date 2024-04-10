00007ac5faaa063d520c9fcbaba8efc35b0bea59
Sina Zinsaz <sinazinsaz1@gamil.com>
2024-04-10 10:51:07 +0330

    feat : update Advertising add compo

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,9 @@ import Head from './components/head/Head'
 import Categories from './components/categories/Categories'
 import Advertising from './components/advertising/Advertising'
 
-
-
-
 export default function App() {
   return (
-    <Box height='100vh'>
+    <Box>
        <Head />
        <Categories />
        <Advertising />
---
+++ src/components/advertising/components/EffectBack.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+
+export default function EffectBack() {
+  return (
+    <Box display='flex' zIndex={-1} position='absolute' height='100%'>
+        <Box component='span' width={{lg:'15rem' , md:"12rem" , xs:'10rem'}} height={{lg:'61%',md:"47%", xs:'39%'}} mt={{ xs: '-2rem', md: '-2rem' , lg:"-4rem" }}
+        borderRadius='30px 30px 0px 0px' style={{background: 'linear-gradient(to top, rgba(90,53,192,0%),rgba(139,98,226,15%))'}}></Box>
+        <Box component='span' width={{lg:'15rem' , md:"12rem" , xs:'10rem'}} height={{lg:'54%',md:'40%' ,xs:'30%'}} mt={{ xs: '2rem', md: '1rem' , lg:'-1rem' }} mr={{ xs:'2rem' , md: '2rem' , lg:'4rem' }}
+        borderRadius='30px 30px 0px 0px' style={{background: 'linear-gradient(to top, rgba(90,53,192,0%),rgba(139,98,226,15%))'}}></Box>
+    </Box>
+  )
+}
