aa8dc518c0ae8d40d666356c809f247a3099e271
Sina Zinsaz <sinazinsaz1@gamil.com>
2024-04-15 00:35:17 +0330

    feat : two Discount

--- src/types/props.ts
+++ src/types/props.ts
@@ -1,21 +1,21 @@
 export type SpanProps = {text:string; color:string}
-export type size = {lg:string; md:string; xs:string}
+export type size = {lg?:string; md?:string; xs?:string}
 export type TextAlign ='left' | 'center' | 'right' | 'justify';
 export type fontWeightSize ='light' | 'regular' | 'medium' | 'bold' | 500 ;
 
 export type BtnProps = {
     Title:string;
-    px?:string;
-    py?:string;
+    px?:size | string;
+    py?:size | string;
     background?:string;
-    color?:string;
-    width?:string;
-    height?:string;
+    color?:size | string;
+    width?:size | string;
+    height?:size | string;
     fontSize?:size | string;
     fontWeight?:fontWeightSize;
-    border?:string;
-    borderRadius?:string;
-    mb?:string;
+    border?:size | string;
+    borderRadius?:size | string;
+    mb?:size | string;
     onClick?: () => void;
 }
 export interface HrProps { 
