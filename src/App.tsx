b41edf4f564067625f990a8acc6663e47a348faa
Sina Zinsaz <sinazinsaz1@gamil.com>
2024-04-21 03:23:45 +0330

    feat: Newsletters

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
-import { Box } from "@mui/material";
+import {
+  Box,
+  IconButton,
+  InputAdornment,
+  TextField,
+  Typography,
+} from "@mui/material";
 import "./index.css";
 import Head from "./components/head/Head";
 import Categories from "./components/categories/Categories";
@@ -9,6 +15,8 @@ import CardProduct from "./components/CardProduct";
 import { ProductsDataTwo } from "./data/Products.data.two";
 import TwoDiscount from "./components/twoDiscount/TwoDiscount";
 import CarouselProducts from "./components/carouselProducts/CarouselProducts";
+import SearchIcon from "@mui/icons-material/Search";
+import Btn from "./components/Btn";
 
 export default function App() {
   return (
@@ -37,8 +45,65 @@ export default function App() {
           ></Box>
         </Box>
       </Box>
-      <Box>Newsletters</Box>
-      <Box>Footer</Box>
+      <Box mx={{ xs: "1rem", md: "4rem" }}>
+        {/* Newsletters */}
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          height="10rem"
+          width="100%"
+        >
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            flexDirection="row"
+            width="100%"
+          >
+            <Box>
+              <Typography fontWeight="bold" mb="0.5rem">
+                ثبت نام در خبرنامه
+              </Typography>
+              <Typography color="#4A4A4A">
+                اولین نفری باشید که از جدیدترین محصولات، جشنواره ها و فروش‌های
+                ویژه ما مطلع می شوید.
+              </Typography>
+            </Box>
+            <Box>
+              <TextField
+                placeholder="شماره موبایل"
+                variant="outlined"
+                dir="rtl"
+                InputProps={{
+                  style: {
+                    fontSize: "1.2rem",
+                    maxWidth: "100%",
+                    borderRadius: "10px",
+                    height: "3rem",
+                    marginLeft: "2rem",
+                  },
+                }}
+                sx={{
+                  width: { xs: "15rem", md: "25rem" },
+                }}
+              />
+              <Btn
+                Title="ارسال"
+                borderRadius="10px"
+                px="2.5rem"
+                border="none"
+                color="white"
+                background="7854F7"
+                py="1rem"
+                fontSize="1rem"
+              />
+            </Box>
+          </Box>
+        </Box>
+        {/* Footer */}
+        <Box>Footer</Box>
+      </Box>
     </Box>
   );
 }
